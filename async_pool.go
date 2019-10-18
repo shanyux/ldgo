dc48532f4dbe01cc9a79c3ee99fa69e51264b66d
distroy <001distroy@gmail.com>
2019-10-19 01:45:24 +0800

    async pool & go pool: added

---
+++ async_pool.go
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) distroy
+ */
+
+package ldgo
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+// Always reference these packages, just in case the auto-generated code below doesn't.
+var _ = bytes.NewBuffer
+var _ = fmt.Sprintf
+var _ = log.New
+var _ = math.Abs
+var _ = os.Exit
+var _ = strconv.Itoa
+var _ = strings.Replace
+var _ = sync.NewCond
+var _ = time.Now
+
+type AsyncPool interface {
+	Async(fn func())
+	Stop()
+	Wait()
+}
+
+func NewAsyncPool(num int) AsyncPool {
+	ap := &asyncPool{
+		ch: make(chan func(), 256),
+	}
+
+	ap.wg.Add(num)
+	for i := 0; i < num; i++ {
+		go ap.main()
+	}
+
+	return ap
+}
+
+type asyncPool struct {
+	once sync.Once
+	wg   sync.WaitGroup
+	ch   chan func()
+}
+
+func (that *asyncPool) Wait() { that.wg.Wait() }
+
+func (that *asyncPool) Stop() {
+	that.once.Do(func() {
+		close(that.ch)
+	})
+}
+
+func (that *asyncPool) Async(fn func()) {
+	that.ch <- fn
+}
+
+func (that *asyncPool) main() {
+	defer that.wg.Done()
+
+	for fn := range that.ch {
+		that.withRecover(fn)
+	}
+}
+
+func (that *asyncPool) withRecover(fn func()) {
+	defer func() {
+		if err := recover(); err != nil {
+		}
+		that.wg.Done()
+	}()
+
+	fn()
+}
---
+++ go_pool.go
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) distroy
+ */
+
+package ldgo
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+// Always reference these packages, just in case the auto-generated code below doesn't.
+var _ = bytes.NewBuffer
+var _ = fmt.Sprintf
+var _ = log.New
+var _ = math.Abs
+var _ = os.Exit
+var _ = strconv.Itoa
+var _ = strings.Replace
+var _ = sync.NewCond
+var _ = time.Now
+
+type GoPool interface {
+	Go(fn func(done <-chan struct{}))
+	Stop()
+	Wait()
+}
+
+func NewGoPool() GoPool {
+	return &goPool{
+		done: make(chan struct{}),
+	}
+}
+
+type goPool struct {
+	wg   sync.WaitGroup
+	once sync.Once
+	done chan struct{}
+}
+
+func (that *goPool) Wait() { that.wg.Wait() }
+
+func (that *goPool) Stop() {
+	that.once.Do(func() {
+		close(that.done)
+	})
+}
+
+func (that *goPool) Go(fn func(done <-chan struct{})) {
+	that.wg.Add(1)
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+			}
+			that.wg.Done()
+		}()
+
+		fn(that.done)
+	}()
+}
