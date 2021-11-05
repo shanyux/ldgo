9ff456d435ff047f5e99d91ed71e21f5dfffff35
distroy <001distroy@gmail.com>
2021-11-05 23:22:39 +0800

    ldlogger: redirected to ldlog

--- logger/logger.go
+++ logger/logger.go
@@ -5,35 +5,31 @@
 package logger
 
 import (
-	"github.com/distroy/ldgo/ldlogger"
+	"github.com/distroy/ldgo/ldlog"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-type Logger = ldlogger.Logger
-
-func GetLogger(log *zap.Logger, fields ...zap.Field) Logger {
-	return ldlogger.GetLogger(log, fields...)
-}
-
-const (
-	LOG_LEVEL         = ldlogger.LOG_LEVEL
-	LOG_ENABLE_CALLER = ldlogger.LOG_ENABLE_CALLER
+type (
+	Logger = ldlog.LoggerInterface
+	Option = ldlog.Option
 )
 
-type Option = ldlogger.Option
-
-func NewLogger(opts ...Option) Logger {
-	return ldlogger.NewLogger(opts...)
+func GetLogger(log *zap.Logger, fields ...zap.Field) Logger {
+	return ldlog.GetLogger(log, fields...)
 }
 
-func Default() Logger { return ldlogger.Default() }
-func Console() Logger { return ldlogger.Console() }
+func NewLogger(opts ...Option) Logger {
+	return ldlog.NewLogger(opts...)
+}
+
+func Default() Logger { return ldlog.Default() }
+func Console() Logger { return ldlog.Console() }
 
 // NewLoggerEncoder
 // log format: 2013-04-08 15:30:42.621|FATAL|0x7f865e4b9720|GdpProcessor.cpp(35)|CGdpPrcessor::Init|init_db_client_fail|id=95,db=gpp_db,type=1
 func NewLoggerEncoder(cfg zapcore.EncoderConfig) zapcore.Encoder {
-	return ldlogger.NewLoggerEncoder(cfg)
+	return ldlog.NewLoggerEncoder(cfg)
 }
 
-type LoggerWrap = ldlogger.LoggerWrapper
+type LoggerWrap = ldlog.WrapperInterface
