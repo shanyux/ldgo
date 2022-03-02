23a4559aa4e614bfe66a389cb8cba37d17d19fe8
distroy <001distroy@gmail.com>
2022-03-02 10:35:52 +0800

    ldtopk: added topk struct

--- ldtopk/topk.go
+++ ldtopk/topk.go
@@ -4,60 +4,85 @@
 
 package ldtopk
 
-type TopkInterface interface {
-	Len() int
-	Less(i, j interface{}) bool
-	Swap(i, j int)
+import "github.com/distroy/ldgo/ldcmp"
 
-	Push(x interface{})
-	Get(i int) interface{}
-	Set(i int, x interface{})
+type LessFunc = func(a, b interface{}) bool
+
+func DefaultLess(a, b interface{}) bool {
+	return ldcmp.CompareInterface(a, b) < 0
 }
 
-func TopkAdd(b TopkInterface, k int, x interface{}) bool {
-	if pos := b.Len(); pos < k {
-		topkAppend(b, x)
-		return true
-	}
+// TopK will keep at most size elements for which less returns true
+type TopK struct {
+	Size int           // K
+	Less LessFunc      // less func
+	data []interface{} //
+}
 
-	if !b.Less(x, b.Get(0)) {
+func (p *TopK) Data() []interface{} { return p.data }
+
+func (p *TopK) Add(d interface{}) bool {
+	k := p.Size
+	if k <= 0 {
 		return false
 	}
 
-	b.Set(0, x)
+	p.init()
+	if pos := len(p.data); pos < k {
+		headAppendTail(&p.data, p.Less, d)
+		return true
+	}
 
+	if !p.Less(d, p.data[0]) {
+		return false
+	}
+	p.data[0] = d
+
+	heapFixupHead(&p.data, p.Less)
+	return true
+}
+
+func (p *TopK) init() {
+	if p.Less == nil {
+		p.Less = DefaultLess
+	}
+	if p.data == nil && p.Size > 0 {
+		p.data = make([]interface{}, 0, p.Size)
+	}
+}
+
+func headAppendTail(heap *[]interface{}, less LessFunc, d interface{}) {
+	pos := len(*heap)
+
+	*heap = append(*heap, d)
+	for parent := 0; pos > 0; pos = parent {
+		parent = (pos - 1) / 2
+		if !less((*heap)[parent], (*heap)[pos]) {
+			break
+		}
+		(*heap)[parent], (*heap)[pos] = (*heap)[pos], (*heap)[parent]
+	}
+}
+
+func heapFixupHead(heap *[]interface{}, less LessFunc) {
+	size := len(*heap)
 	for pos := 0; ; {
 		lChild := (pos * 2) + 1
 		rChild := (pos * 2) + 2
-		if lChild >= k {
+		if lChild >= size {
 			break
 		}
 
 		child := lChild
-		if rChild < k && b.Less(b.Get(lChild), b.Get(rChild)) {
+		if rChild < size && less((*heap)[lChild], (*heap)[rChild]) {
 			child = rChild
 		}
 
-		if !b.Less(b.Get(pos), b.Get(child)) {
+		if !less((*heap)[pos], (*heap)[child]) {
 			break
 		}
 
-		b.Swap(pos, child)
+		(*heap)[pos], (*heap)[child] = (*heap)[child], (*heap)[pos]
 		pos = child
-	}
-
-	return true
-}
-
-func topkAppend(b TopkInterface, x interface{}) {
-	pos := b.Len()
-	b.Push(x)
-
-	for parent := 0; pos > 0; pos = parent {
-		parent = (pos - 1) / 2
-		if !b.Less(b.Get(parent), b.Get(pos)) {
-			break
-		}
-		b.Swap(parent, pos)
 	}
 }
