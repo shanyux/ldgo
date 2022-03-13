13314e193f9c535112b4301fd2a77ca5bce339ee
distroy <001distroy@gmail.com>
2022-03-13 19:44:50 +0800

    ldconv: unit test

--- ldconv/conv_number_test.go
+++ ldconv/conv_number_test.go
@@ -17,31 +17,29 @@ const (
 
 func testConvInt(s string, r int64) {
 	convey.Convey(s, func() {
-		n, _ := convInt(StrToBytesUnsafe(s))
-		convey.So(n, convey.ShouldEqual, r)
+		convey.So(AsInt64(s), convey.ShouldEqual, r)
 	})
 }
 
 func testConvUint(s string, r uint64) {
 	convey.Convey(s, func() {
-		n, _ := convUint(StrToBytesUnsafe(s))
-		convey.So(n, convey.ShouldEqual, r)
+		convey.So(AsUint64(s), convey.ShouldEqual, r)
 	})
 }
 
-func testConvFloat(s string, r0 float64, r1 int64) {
-	name := fmt.Sprintf("%s|%g|%d", s, r0, r1)
+func testConvFloat(s string, rf32 float32, rf64 float64, ri64 int64) {
+	name := fmt.Sprintf("%s|%g|%g|%d", s, rf32, rf64, ri64)
 	convey.Convey(name, func() {
-		n0, _ := convFloat(StrToBytesUnsafe(s))
-		v0, _ := n0.Float64()
-		convey.So(v0, convey.ShouldEqual, r0)
 
-		baseFloat := newDecimalFromInt(_TEST_FLOAT_MUL_BASE)
+		convey.So(AsFloat32(s), convey.ShouldEqual, rf32)
+
+		convey.So(AsFloat64(s), convey.ShouldEqual, rf64)
+
+		n0 := asDecimal(s)
+		baseFloat := asDecimal(_TEST_FLOAT_MUL_BASE)
 		n1 := n0.Mul(baseFloat)
-		// x, _ := (&big.Float{}).SetString(strconv.FormatFloat(r, 'f', -1, 64))
-		// x, _ := decimal.NewFromString(s)
-		f1, _ := n1.BigFloat().Int64()
-		convey.So(f1, convey.ShouldEqual, r1)
+		vi64, _ := n1.BigFloat().Int64()
+		convey.So(vi64, convey.ShouldEqual, ri64)
 	})
 }
 
@@ -86,28 +84,28 @@ func TestConvUint(t *testing.T) {
 
 func TestConvFloat(t *testing.T) {
 	convey.Convey("", t, func() {
-		testConvFloat("0", 0, 0*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("9.9", 9.9, 9.9*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("20.09", 20.09, 20.09*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("19.99", 19.99, 19.99*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("0.99", 0.99, 0.99*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("2.32", 2.32, 2.32*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("123", 123, 123*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("123.1", 123.1, 123.1*_TEST_FLOAT_MUL_BASE)
-		testConvFloat(".1", 0.1, 0.1*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("0123", 123, 123*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("0o123", 0123, 0123*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("0", 0, 0, 0*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("9.9", 9.9, 9.9, 9.9*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("20.09", 20.09, 20.09, 20.09*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("19.99", 19.99, 19.99, 19.99*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("0.99", 0.99, 0.99, 0.99*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("2.32", 2.32, 2.32, 2.32*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("123", 123, 123, 123*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("123.1", 123.1, 123.1, 123.1*_TEST_FLOAT_MUL_BASE)
+		testConvFloat(".1", 0.1, 0.1, 0.1*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("0123", 123, 123, 123*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("0o123", 0123, 0123, 0123*_TEST_FLOAT_MUL_BASE)
 		// testConvFloat("+-0123", uint64(int64(-0123)))
-		testConvFloat("123e+1", 1230, 1230*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("123e1", 1230, 1230*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("123e-1", 12.3, 12.3*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("0x123", 0x123, 0x123*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("--0X123", 0x123, 0x123*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("-+-0x123", 0x123, 0x123*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("-+-0xeFaB", 0xeFaB, 0xeFaB*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("---0xeFaB", -0xeFaB, -0xeFaB*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("0.30129", 0.30129, 0.30129*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("0.30129e3", 301.29, 301.29*_TEST_FLOAT_MUL_BASE)
-		testConvFloat("-0.30129E-3", -0.00030129, -30)
+		testConvFloat("123e+1", 1230, 1230, 1230*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("123e1", 1230, 1230, 1230*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("123e-1", 12.3, 12.3, 12.3*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("0x123", 0x123, 0x123, 0x123*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("--0X123", 0x123, 0x123, 0x123*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("-+-0x123", 0x123, 0x123, 0x123*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("-+-0xeFaB", 0xeFaB, 0xeFaB, 0xeFaB*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("---0xeFaB", -0xeFaB, -0xeFaB, -0xeFaB*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("0.30129", 0.30129, 0.30129, 0.30129*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("0.30129e3", 301.29, 301.29, 301.29*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("-0.30129E-3", -0.00030129, -0.00030129, -30)
 	})
 }
