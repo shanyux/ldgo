c77ef5d2876988f8042d5e2187c354dc554db7d7
distroy <distroy@163.com>
2022-08-09 14:58:16 +0800

    test(ldref): unit test

--- ldref/is_zero_test.go
+++ ldref/is_zero_test.go
@@ -6,6 +6,7 @@ package ldref
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/smartystreets/goconvey/convey"
 )
@@ -48,13 +49,48 @@ func TestIsZero(t *testing.T) {
 		})
 
 		convey.Convey("[...]int", func() {
-			convey.So(IsZero([3]int{}), convey.ShouldEqual, true)
-			convey.So(IsZero([3]int{0, 0, 1}), convey.ShouldEqual, false)
+			convey.So(IsZero([...]int{}), convey.ShouldEqual, true)
+			convey.So(IsZero([...]int{0, 0, 1}), convey.ShouldEqual, false)
 		})
 
 		convey.Convey("string", func() {
 			convey.So(IsZero(""), convey.ShouldEqual, true)
 			convey.So(IsZero("1"), convey.ShouldEqual, false)
+		})
+
+		convey.Convey("complex128", func() {
+			convey.So(IsZero(complex(0, 0)), convey.ShouldEqual, true)
+			convey.So(IsZero(complex(0, 1)), convey.ShouldEqual, false)
+			convey.So(IsZero(complex(1, 0)), convey.ShouldEqual, false)
+		})
+
+		convey.Convey("unsafe.Pointer", func() {
+			n := 1
+			convey.So(IsZero((unsafe.Pointer)(nil)), convey.ShouldEqual, true)
+			convey.So(IsZero((unsafe.Pointer)(&n)), convey.ShouldEqual, false)
+		})
+
+		convey.Convey("map[int]struct{}", func() {
+			convey.So(IsZero((map[int]struct{})(nil)), convey.ShouldEqual, true)
+			convey.So(IsZero(map[int]struct{}{}), convey.ShouldEqual, false)
+		})
+
+		convey.Convey("func ()", func() {
+			convey.So(IsZero((func())(nil)), convey.ShouldEqual, true)
+			convey.So(IsZero(func() {}), convey.ShouldEqual, false)
+		})
+
+		convey.Convey("chan struct{}", func() {
+			convey.So(IsZero((chan struct{})(nil)), convey.ShouldEqual, true)
+			convey.So(IsZero(make(chan struct{})), convey.ShouldEqual, false)
+		})
+
+		convey.Convey("struct", func() {
+			type testStruct = struct {
+				Int int
+			}
+			convey.So(IsZero(testStruct{}), convey.ShouldEqual, true)
+			convey.So(IsZero(testStruct{Int: 1}), convey.ShouldEqual, false)
 		})
 	})
 }
