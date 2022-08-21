a317142c788a5c18ceceacfd67f9e04502e87819
distroy <001distroy@gmail.com>
2022-08-21 23:40:26 +0800

    test(ldcmp): unit test

--- ldcmp/compare_test.go
+++ ldcmp/compare_test.go
@@ -7,6 +7,7 @@ package ldcmp
 import (
 	"math"
 	"testing"
+	"time"
 
 	"github.com/smartystreets/goconvey/convey"
 )
@@ -92,5 +93,206 @@ func TestCompareInterface(t *testing.T) {
 				[]interface{}{100, float64(200), ""},
 			), convey.ShouldEqual, 0)
 		})
+	})
+}
+
+func TestCompareBool(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareBool(false, false), convey.ShouldEqual, 0)
+		convey.So(CompareBool(true, true), convey.ShouldEqual, 0)
+		convey.So(CompareBool(false, true), convey.ShouldEqual, -1)
+		convey.So(CompareBool(true, false), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareByte(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareByte(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareByte(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareByte(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareByte(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareRune(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareRune(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareRune(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareRune(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareRune(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareInt(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareInt(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareInt(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareInt(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareInt(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareInt8(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareInt8(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareInt8(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareInt8(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareInt8(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareInt16(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareInt16(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareInt16(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareInt16(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareInt16(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareInt32(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareInt32(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareInt32(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareInt32(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareInt32(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareInt64(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareInt64(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareInt64(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareInt64(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareInt64(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareUint(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareUint(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareUint(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareUint(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareUint(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareUint8(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareUint8(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareUint8(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareUint8(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareUint8(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareUint16(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareUint16(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareUint16(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareUint16(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareUint16(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareUint32(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareUint32(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareUint32(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareUint32(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareUint32(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareUint64(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareUint64(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareUint64(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareUint64(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareUint64(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareFloat32(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareFloat32(float32(math.NaN()), float32(math.NaN())), convey.ShouldEqual, 0)
+		convey.So(CompareFloat32(0, float32(math.NaN())), convey.ShouldEqual, 1)
+		convey.So(CompareFloat32(float32(math.NaN()), 0), convey.ShouldEqual, -1)
+
+		convey.So(CompareFloat32(float32(math.Inf(-1)), float32(math.NaN())), convey.ShouldEqual, 1)
+		convey.So(CompareFloat32(float32(math.NaN()), float32(math.Inf(-1))), convey.ShouldEqual, -1)
+
+		convey.So(CompareFloat32(float32(math.Inf(1)), float32(math.Inf(1))), convey.ShouldEqual, 0)
+		convey.So(CompareFloat32(float32(math.Inf(-1)), float32(math.Inf(-1))), convey.ShouldEqual, 0)
+
+		convey.So(CompareFloat32(float32(math.Inf(-1)), float32(math.Inf(1))), convey.ShouldEqual, -1)
+		convey.So(CompareFloat32(float32(math.Inf(1)), float32(math.Inf(-1))), convey.ShouldEqual, 1)
+
+		convey.So(CompareFloat32(float32(math.Inf(-1)), 0), convey.ShouldEqual, -1)
+		convey.So(CompareFloat32(0, float32(math.Inf(-1))), convey.ShouldEqual, 1)
+		convey.So(CompareFloat32(float32(math.Inf(1)), 0), convey.ShouldEqual, 1)
+		convey.So(CompareFloat32(0, float32(math.Inf(1))), convey.ShouldEqual, -1)
+
+		convey.So(CompareFloat32(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareFloat32(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareFloat32(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareFloat32(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareFloat64(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareFloat64(math.NaN(), math.NaN()), convey.ShouldEqual, 0)
+		convey.So(CompareFloat64(0, math.NaN()), convey.ShouldEqual, 1)
+		convey.So(CompareFloat64(math.NaN(), 0), convey.ShouldEqual, -1)
+
+		convey.So(CompareFloat64(math.Inf(-1), math.NaN()), convey.ShouldEqual, 1)
+		convey.So(CompareFloat64(math.NaN(), math.Inf(-1)), convey.ShouldEqual, -1)
+
+		convey.So(CompareFloat64(math.Inf(1), math.Inf(1)), convey.ShouldEqual, 0)
+		convey.So(CompareFloat64(math.Inf(-1), math.Inf(-1)), convey.ShouldEqual, 0)
+
+		convey.So(CompareFloat64(math.Inf(-1), math.Inf(1)), convey.ShouldEqual, -1)
+		convey.So(CompareFloat64(math.Inf(1), math.Inf(-1)), convey.ShouldEqual, 1)
+
+		convey.So(CompareFloat64(math.Inf(-1), 0), convey.ShouldEqual, -1)
+		convey.So(CompareFloat64(0, math.Inf(-1)), convey.ShouldEqual, 1)
+		convey.So(CompareFloat64(math.Inf(1), 0), convey.ShouldEqual, 1)
+		convey.So(CompareFloat64(0, math.Inf(1)), convey.ShouldEqual, -1)
+
+		convey.So(CompareFloat64(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareFloat64(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareFloat64(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareFloat64(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestString(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareString(``, ""), convey.ShouldEqual, 0)
+		convey.So(CompareString(`aaa`, "aaa"), convey.ShouldEqual, 0)
+
+		convey.So(CompareString(`aaa`, ""), convey.ShouldEqual, 1)
+		convey.So(CompareString(``, "aaa"), convey.ShouldEqual, -1)
+		convey.So(CompareString(`abc`, "aaa"), convey.ShouldEqual, 1)
+		convey.So(CompareString(`aa`, "aaa"), convey.ShouldEqual, -1)
+	})
+}
+
+func TestCompareDuration(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareDuration(0, 0), convey.ShouldEqual, 0)
+		convey.So(CompareDuration(123, 123), convey.ShouldEqual, 0)
+		convey.So(CompareDuration(0, 123), convey.ShouldEqual, -1)
+		convey.So(CompareDuration(123, 0), convey.ShouldEqual, 1)
+	})
+}
+
+func TestCompareTime(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(CompareTime(time.Unix(0, 0), time.Unix(0, 0)), convey.ShouldEqual, 0)
+		convey.So(CompareTime(time.Unix(123, 0), time.Unix(123, 0)), convey.ShouldEqual, 0)
+		convey.So(CompareTime(time.Unix(0, 0), time.Unix(123, 0)), convey.ShouldEqual, -1)
+		convey.So(CompareTime(time.Unix(123, 0), time.Unix(0, 0)), convey.ShouldEqual, 1)
 	})
 }
