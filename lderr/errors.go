fba4a9038d1c106ff874ba75fe81501be01b3a22
distroy <distroy@163.com>
2023-02-01 15:40:13 +0800

    feat(ldconv): use the error in package lderr

--- lderr/errors.go
+++ lderr/errors.go
@@ -20,7 +20,9 @@ var (
 	ErrReflectTargetNilPtr = New(http.StatusOK, -23, "reflect target is nil pointer")
 	ErrReflectTypeNotEqual = New(http.StatusOK, -24, "reflect types of target and source are not equal")
 
-	ErrNumberOverflow = New(http.StatusOK, -31, "number overflow")
+	ErrNumberOverflow      = New(http.StatusOK, -31, "number overflow")
+	ErrInvalidNumberSyntax = New(http.StatusOK, -32, "invalid number syntax")
+	ErrInvalidConvertType  = New(http.StatusOK, -33, "invalid convert type")
 
 	ErrNonAuthoritativeInfo = New(http.StatusNonAuthoritativeInfo, -101, "http non authoritative info")
 	ErrUnauthorized         = New(http.StatusUnauthorized, -102, "http unauthorized")
