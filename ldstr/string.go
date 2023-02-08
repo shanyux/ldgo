612a15598cfc7ec63ba88bde0ee7856cdf333bee
distroy <distroy@163.com>
2023-02-08 15:23:37 +0800

    feat(ldstr): doc for str map replace

--- ldstr/string.go
+++ ldstr/string.go
@@ -11,10 +11,28 @@ import (
 	"github.com/distroy/ldgo/ldconv"
 )
 
-func StrMapReplace(s string, m map[string]string, l string, r string) string {
-	if len(m) == 0 {
-		return s
+func getReplaceSplits(splits []string) (l, r string) {
+	switch len(splits) {
+	case 0:
+		return "{", "}"
+	case 1:
+		return splits[0], splits[0]
+	default:
+		return splits[0], splits[1]
 	}
+}
+
+// StrMapReplace returns new string.
+//
+//	StrMapReplace("user: {user}", map[string]string{"user", "x"}) returns "user: x"
+//	StrMapReplace("user: #user#", map[string]string{"user", "x"}, "#") returns "user: x"
+//	StrMapReplace("user: <user>", map[string]string{"user", "<", ">"}, "#") returns "user: x"
+func StrMapReplace(tmpl string, m map[string]string, splits ...string) string {
+	if len(m) == 0 {
+		return tmpl
+	}
+
+	l, r := getReplaceSplits(splits)
 
 	replaceKeyValues := make([]string, 0, len(m)*2)
 	for k, v := range m {
@@ -29,15 +47,22 @@ func StrMapReplace(s string, m map[string]string, l string, r string) string {
 	}
 
 	replacer := strings.NewReplacer(replaceKeyValues...)
-	return replacer.Replace(s)
+	return replacer.Replace(tmpl)
 }
 
-func StrIMapReplace(s string, _m interface{}, l string, r string) string {
-	if m, ok := _m.(map[string]string); ok {
-		return StrMapReplace(s, m, l, r)
+// StrIMapReplace returns new string.
+//
+//	StrIMapReplace("user: {user}", map[interface{}]interface{}{"user", "x"}) returns "user: x"
+//	StrIMapReplace("user: #user#", map[interface{}]interface{}{"user", "x"}, "#") returns "user: x"
+//	StrIMapReplace("user: <user>", map[interface{}]interface{}{"user", "<", ">"}, "#") returns "user: x"
+func StrIMapReplace(tmpl string, m interface{}, splits ...string) string {
+	if m, ok := m.(map[string]string); ok {
+		return StrMapReplace(tmpl, m, splits...)
 	}
 
-	val := reflect.ValueOf(_m)
+	l, r := getReplaceSplits(splits)
+
+	val := reflect.ValueOf(m)
 	for val.Kind() == reflect.Ptr {
 		if val.IsNil() {
 			return ""
@@ -48,7 +73,7 @@ func StrIMapReplace(s string, _m interface{}, l string, r string) string {
 		return ""
 	}
 	if val.Len() == 0 {
-		return s
+		return tmpl
 	}
 
 	replaceKeyValues := make([]string, 0, val.Len()*2)
@@ -67,5 +92,5 @@ func StrIMapReplace(s string, _m interface{}, l string, r string) string {
 	}
 
 	replacer := strings.NewReplacer(replaceKeyValues...)
-	return replacer.Replace(s)
+	return replacer.Replace(tmpl)
 }
