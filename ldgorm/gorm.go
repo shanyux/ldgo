f3e34eaabc1083fe60f6c64061795ce0d40139cc
distroy <001distroy@gmail.com>
2023-12-19 23:20:14 +0800

    feat(ldgorm): add func NewTestGormDb

--- ldgorm/gorm.go
+++ ldgorm/gorm.go
@@ -5,8 +5,11 @@
 package ldgorm
 
 import (
+	"fmt"
+
 	gorm2 "github.com/distroy/ldgo/ldgorm/internal/jinzhu/gorm"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 type GormDb = gorm2.GormDb
@@ -15,4 +18,22 @@ func NewGormDb(db *gorm.DB) *GormDb {
 	g := &GormDb{}
 	g = g.Set(db)
 	return g
+}
+
+func NewTestGormDb() (*GormDb, error) {
+	db, err := gorm.Open("sqlite3", ":memory:")
+	if err != nil {
+		return nil, err
+	}
+
+	return NewGormDb(db), nil
+}
+
+func MustNewTestGormDb() *GormDb {
+	db, err := NewTestGormDb()
+	if err != nil {
+		panic(fmt.Sprintf("new test gorm fail. err:%v", err))
+	}
+
+	return db
 }
