062b202f0a7a6f5d31166798d12bf2665e5e7ced
distroy <distroy@163.com>
2024-01-29 12:23:18 +0800

    feat(ldatomic): compare and swap for time

--- ldatomic/time_test.go
+++ ldatomic/time_test.go
@@ -21,22 +21,26 @@ func testFixedTimezone(name string, tm time.Duration) *time.Location {
 func TestTime(t *testing.T) {
 	const format = "2006-01-02T15:04:05-0700"
 	now := time.Unix(1644479966, 0).In(_testTimezone)
-	convey.Convey(t.Name(), t, func() {
-		convey.So(NewTime(time.Time{}).Load().Format(format), convey.ShouldEqual, _zeroTime.Format(format))
+	convey.Convey(t.Name(), t, func(c convey.C) {
+		c.So(NewTime(time.Time{}).Load().Format(format), convey.ShouldEqual, _zeroTime.Format(format))
 
 		p := Time{}
-		convey.So(p.Load().Format(format), convey.ShouldEqual, _zeroTime.Format(format))
+		c.So(p.Load().Format(format), convey.ShouldEqual, _zeroTime.Format(format))
 
 		p.Store(now)
-		convey.So(p.Load().Format(format), convey.ShouldEqual, "2022-02-10T15:59:26+0800")
+		c.So(p.Load().Format(format), convey.ShouldEqual, "2022-02-10T15:59:26+0800")
 
 		p.Add(60 * time.Second)
-		convey.So(p.Load().Format(format), convey.ShouldEqual, "2022-02-10T16:00:26+0800")
+		c.So(p.Load().Format(format), convey.ShouldEqual, "2022-02-10T16:00:26+0800")
 
 		p.AddDate(-1, 1, 3)
-		convey.So(p.Load().Format(format), convey.ShouldEqual, "2021-03-13T16:00:26+0800")
+		c.So(p.Load().Format(format), convey.ShouldEqual, "2021-03-13T16:00:26+0800")
 
-		convey.So(p.Swap(now).Format(format), convey.ShouldEqual, "2021-03-13T16:00:26+0800")
-		convey.So(p.Load().Format(format), convey.ShouldEqual, "2022-02-10T15:59:26+0800")
+		c.So(p.Swap(now).Format(format), convey.ShouldEqual, "2021-03-13T16:00:26+0800")
+		c.So(p.Load().Format(format), convey.ShouldEqual, "2022-02-10T15:59:26+0800")
+
+		c.So(p.CompareAndSwap(time.Time{}, now), convey.ShouldBeFalse)
+		c.So(p.CompareAndSwap(now, time.Time{}), convey.ShouldBeTrue)
+		c.So(p.CompareAndSwap(time.Time{}, now), convey.ShouldBeTrue)
 	})
 }
