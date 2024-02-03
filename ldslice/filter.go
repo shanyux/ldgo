8ef9c1d0d923adb52072c0dcc1608e21859cdfc9
distroy <001distroy@gmail.com>
2024-02-03 21:34:32 +0800

    refactor(ldslice): refactor func Filter

--- ldslice/filter.go
+++ ldslice/filter.go
@@ -4,73 +4,35 @@
 
 package ldslice
 
-import (
-	"fmt"
-	"reflect"
-)
-
-// Filter filters the slice with filter
-// slice type must be slice or array
-// filter type must be:
-//
-//	func (v TypeOfSliceElement) bool
-func Filter(slice interface{}, filter interface{}) int {
-	sliceVal := reflect.ValueOf(slice)
-	sliceTyp := sliceVal.Type()
-
-	if sliceTyp.Kind() != reflect.Slice && sliceTyp.Kind() != reflect.Array {
-		panic(fmt.Sprintf("the slice type must be slice or array. type:%s", sliceTyp))
-	}
-	elemTyp := sliceVal.Type().Elem()
-
-	filterVal := reflect.ValueOf(filter)
-	filterTyp := filterVal.Type()
-	if filterTyp.Kind() != reflect.Func {
-		panic(fmt.Sprintf("the filter must be func. type:%s", filterTyp))
-	}
-
-	if filterTyp.NumIn() != 1 {
-		panic(fmt.Sprintf("the filter must have 1 input parameter. type:%s", filterTyp))
-	}
-	if typ := filterTyp.In(0); !(typ == elemTyp || (typ.Kind() == reflect.Interface && elemTyp.Implements(typ))) {
-		panic(fmt.Sprintf("the parameter of filter must be or interface for %s", elemTyp))
-	}
-
-	if filterTyp.NumOut() != 1 {
-		panic(fmt.Sprintf("the filter must have 1 return value. type:%s", filterTyp))
-	}
-	if typ := filterTyp.Out(0); typ.Kind() != reflect.Bool {
-		panic(fmt.Sprintf("the return value of filter must be bool. type:%s", typ))
-	}
-
-	return filterSlice(sliceVal, filterVal)
+// Filter returns the slice with filter(v) == true
+func Filter[T any](slice []T, filter func(v T) bool) []T {
+	n := filterSlice(slice, filter)
+	return slice[:n]
 }
 
-func filterSlice(slice, filter reflect.Value) int {
+func filterSlice[T any](slice []T, filter func(v T) bool) int {
 	i := 0
-	j := slice.Len()
+	j := len(slice)
 
 	for i < j {
-		var vi, vj reflect.Value
+		var vi, vj T
 
 		for ; i < j; i++ {
-			vi = slice.Index(i)
-			if !filter.Call([]reflect.Value{vi})[0].Bool() {
+			vi = slice[i]
+			if !filter(vi) {
 				break
 			}
 		}
 
 		for ; i < j; j-- {
-			vj = slice.Index(j - 1)
-			if filter.Call([]reflect.Value{vj})[0].Bool() {
+			vj = slice[j-1]
+			if filter(vj) {
 				break
 			}
 		}
 
 		if i < j-1 {
-			tmp := vi.Interface()
-			vi.Set(vj)
-			vj.Set(reflect.ValueOf(tmp))
+			vi, vj = vj, vi
 			i++
 		}
 	}
