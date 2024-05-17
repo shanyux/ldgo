30a6d6adc6a4796f0c4cc0ccad7a841b95320a12
distroy <distroy@163.com>
2024-05-17 12:12:15 +0800

    feat(ldctx): always print log at error & fatal level

--- ldctx/unexported.go
+++ ldctx/unexported.go
@@ -44,14 +44,17 @@ func zSugar(c context.Context, skip int) *zap.SugaredLogger {
 	return l.Sugar()
 }
 
+func lCore(c context.Context) *zap.Logger         { return GetLogger(c).Core() }
+func lSugar(c context.Context) *zap.SugaredLogger { return GetLogger(c).Sugar() }
+
 func LogD(c context.Context, msg string, fields ...zap.Field) { zCore(c, 1).Debug(msg, fields...) }
 func LogI(c context.Context, msg string, fields ...zap.Field) { zCore(c, 1).Info(msg, fields...) }
 func LogW(c context.Context, msg string, fields ...zap.Field) { zCore(c, 1).Warn(msg, fields...) }
-func LogE(c context.Context, msg string, fields ...zap.Field) { zCore(c, 1).Error(msg, fields...) }
-func LogF(c context.Context, msg string, fields ...zap.Field) { zCore(c, 1).Fatal(msg, fields...) }
+func LogE(c context.Context, msg string, fields ...zap.Field) { lCore(c).Error(msg, fields...) }
+func LogF(c context.Context, msg string, fields ...zap.Field) { lCore(c).Fatal(msg, fields...) }
 
 func LogDf(c context.Context, fmt string, args ...interface{}) { zSugar(c, 1).Debugf(fmt, args...) }
 func LogIf(c context.Context, fmt string, args ...interface{}) { zSugar(c, 1).Infof(fmt, args...) }
 func LogWf(c context.Context, fmt string, args ...interface{}) { zSugar(c, 1).Warnf(fmt, args...) }
-func LogEf(c context.Context, fmt string, args ...interface{}) { zSugar(c, 1).Errorf(fmt, args...) }
-func LogFf(c context.Context, fmt string, args ...interface{}) { zSugar(c, 1).Fatalf(fmt, args...) }
+func LogEf(c context.Context, fmt string, args ...interface{}) { lSugar(c).Errorf(fmt, args...) }
+func LogFf(c context.Context, fmt string, args ...interface{}) { lSugar(c).Fatalf(fmt, args...) }
