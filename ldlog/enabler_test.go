ecaae130dccee6f952f78667f9ac69acbfff8909
distroy <distroy@163.com>
2024-06-27 14:59:25 +0800

    test(ldlog): unit test

--- ldlog/enabler_test.go
+++ ldlog/enabler_test.go
@@ -12,11 +12,64 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-func TestRateEnabler_Enable(t *testing.T) {
+func TestRateEnabler(t *testing.T) {
+	convey.Convey(t.Name(), t, func(c convey.C) {
+		c.Convey("1", func(c convey.C) {
+			l := RateEnabler(1)
+			c.So(l, convey.ShouldResemble, defaultEnabler{})
+		})
+		c.Convey("0", func(c convey.C) {
+			l := RateEnabler(0)
+			c.So(l, convey.ShouldResemble, falseEnabler{})
+		})
+		c.Convey("0.5", func(c convey.C) {
+			l := RateEnabler(0.5)
+			c.So(l, convey.ShouldResemble, rateEnabler{rate: 0.5})
+
+			testRateEnabler_HalfEnable(c, l)
+		})
+	})
+}
+
+func TestIntervalEnabler(t *testing.T) {
+	convey.Convey(t.Name(), t, func(c convey.C) {
+		c.Convey("0", func(c convey.C) {
+			l := IntervalEnabler(0)
+			c.So(l, convey.ShouldResemble, defaultEnabler{})
+		})
+		c.Convey("50ms", func(c convey.C) {
+			const (
+				info = zapcore.InfoLevel
+				err  = zapcore.ErrorLevel
+
+				interval = time.Millisecond * 50
+			)
+
+			l := IntervalEnabler(interval)
+			c.So(l, convey.ShouldResemble, intervalEnabler{interval: interval})
+
+			time.Sleep(interval)
+			c.So(l.Enable(info, 1), convey.ShouldBeTrue)
+			c.So(l.Enable(err, 1), convey.ShouldBeTrue)
+			for i := 0; i < 100; i++ {
+				c.So(l.Enable(info, 1), convey.ShouldBeFalse)
+				c.So(l.Enable(err, 1), convey.ShouldBeTrue)
+			}
+			time.Sleep(interval)
+			c.So(l.Enable(err, 1), convey.ShouldBeTrue)
+			c.So(l.Enable(info, 1), convey.ShouldBeTrue)
+			c.So(l.Enable(info, 1), convey.ShouldBeFalse)
+		})
+	})
+}
+
+func Test_rateEnabler_Enable(t *testing.T) {
 	convey.Convey(t.Name(), t, func(c convey.C) {
 		l := &rateEnabler{}
-		info := zapcore.InfoLevel
-		err := zapcore.ErrorLevel
+		const (
+			info = zapcore.InfoLevel
+			err  = zapcore.ErrorLevel
+		)
 
 		c.Convey("1", func(c convey.C) {
 			l.rate = 1
@@ -33,24 +86,38 @@ func TestRateEnabler_Enable(t *testing.T) {
 		})
 		c.Convey("0.5", func(c convey.C) {
 			l.rate = 0.5
-			var (
-				totalCnt = 20000
-				diff     = 1000
-			)
-			trueCnt := 0
-			for i := 0; i < totalCnt; i++ {
-				if l.Enable(info) {
-					trueCnt++
-				}
-				c.So(l.Enable(err), convey.ShouldBeTrue)
-			}
-			half := totalCnt / 2
-			c.So(trueCnt, convey.ShouldBeBetweenOrEqual, half-diff, half+diff)
+			testRateEnabler_HalfEnable(c, l)
 		})
 	})
 }
 
-func TestIntervalEnabler_Enable(t *testing.T) {
+func testRateEnabler_HalfEnable(c convey.C, l Enabler) {
+	const (
+		info = zapcore.InfoLevel
+		err  = zapcore.ErrorLevel
+	)
+	c.Convey("info log", func(c convey.C) {
+		var (
+			totalCnt = 20000
+			diff     = 1000
+		)
+		trueCnt := 0
+		for i := 0; i < totalCnt; i++ {
+			if l.Enable(info) {
+				trueCnt++
+			}
+		}
+		half := totalCnt / 2
+		c.So(trueCnt, convey.ShouldBeBetweenOrEqual, half-diff, half+diff)
+	})
+	c.Convey("error log", func(c convey.C) {
+		for i := 0; i < 100; i++ {
+			c.So(l.Enable(err), convey.ShouldBeTrue)
+		}
+	})
+}
+
+func Test_intervalEnabler_Enable(t *testing.T) {
 	convey.Convey(t.Name(), t, func(c convey.C) {
 		l := &intervalEnabler{}
 		info := zapcore.InfoLevel
@@ -63,7 +130,7 @@ func TestIntervalEnabler_Enable(t *testing.T) {
 				c.So(l.Enable(err, 0), convey.ShouldBeTrue)
 			}
 		})
-		c.Convey("1s", func(c convey.C) {
+		c.Convey("50ms", func(c convey.C) {
 			interval := time.Millisecond * 50
 			l.interval = interval
 
