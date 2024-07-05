56804c9bb1aaed2b01b8a527c34ebebf858076dd
distroy <distroy@163.com>
2024-07-05 18:53:55 +0800

    feat(ldmath): add generics funcs

--- ldmath/abs_test.go
+++ ldmath/abs_test.go
@@ -77,3 +77,35 @@ func TestAbsFloat64(t *testing.T) {
 		convey.So(AbsFloat64(Inf64(-1)), convey.ShouldEqual, Inf64(1))
 	})
 }
+
+func TestAbs(t *testing.T) {
+	convey.Convey(t.Name(), t, func(c convey.C) {
+		c.Convey("int", func(c convey.C) {
+			c.So(Abs(0), convey.ShouldEqual, 0)
+			c.So(Abs(100), convey.ShouldEqual, 100)
+			c.So(Abs(-100), convey.ShouldEqual, 100)
+		})
+		c.Convey("float32", func(c convey.C) {
+			c.So(Abs[float32](0), convey.ShouldEqual, 0)
+			c.So(Abs[float32](100), convey.ShouldEqual, 100)
+			c.So(Abs[float32](-100), convey.ShouldEqual, 100)
+
+			c.So(IsNaN(Abs[float32](0)), convey.ShouldResemble, false)
+			c.So(IsNaN(Abs(NaN32())), convey.ShouldResemble, true)
+
+			c.So(IsInf(Abs(Inf32(1)), 1), convey.ShouldEqual, true)
+			c.So(IsInf(Abs(Inf32(-1)), 1), convey.ShouldEqual, true)
+		})
+		c.Convey("float64", func(c convey.C) {
+			c.So(Abs[float64](0), convey.ShouldEqual, 0)
+			c.So(Abs[float64](100), convey.ShouldEqual, 100)
+			c.So(Abs[float64](-100), convey.ShouldEqual, 100)
+
+			c.So(IsNaN(AbsFloat64(0)), convey.ShouldResemble, false)
+			c.So(IsNaN(Abs(NaN64())), convey.ShouldResemble, true)
+
+			c.So(IsInf(Abs(Inf64(1)), -1), convey.ShouldEqual, false)
+			c.So(IsInf(Abs(Inf64(-1)), -1), convey.ShouldEqual, false)
+		})
+	})
+}
