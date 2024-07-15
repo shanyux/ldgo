3dae3af3dbf100e18d65f59ff6586686d88b0eb2
distroy <distroy@163.com>
2024-07-15 16:05:06 +0800

    refactor(ldsync): refactor `Done` & `Once` & `DoneWait`

--- ldsync/done.go
+++ ldsync/done.go
@@ -6,30 +6,41 @@ package ldsync
 
 import (
 	"sync"
+	"sync/atomic"
 )
 
 type none = struct{}
 
-type Done interface {
-	Stop()
-	Chan() <-chan none
+type Done struct {
+	done   chan none
+	mutex  sync.Mutex
+	inited uint32
+	stoped uint32
 }
 
-func NewDone() Done {
-	return &done{
-		c: make(chan struct{}),
-	}
+func (p *Done) Stop() {
+	p.init()
+	stoped := once{done: &p.stoped, mutex: &p.mutex}
+	stoped.Do(func() {
+		close(p.done)
+	})
 }
 
-type done struct {
-	c chan none
-	o sync.Once
+func (p *Done) Chan() <-chan none {
+	p.init()
+	return p.done
 }
 
-func (that *done) Stop() {
-	that.o.Do(func() { close(that.c) })
+func (p *Done) init() {
+	inited := once{done: &p.inited, mutex: &p.mutex}
+	inited.Do(func() {
+		p.done = make(chan struct{})
+	})
 }
 
-func (that *done) Chan() <-chan none {
-	return that.c
+func (p *Done) Reset() {
+	p.mutex.Lock()
+	atomic.StoreUint32(&p.inited, 0)
+	atomic.StoreUint32(&p.stoped, 0)
+	p.mutex.Unlock()
 }
--- ldsync/done_wait.go
+++ ldsync/done_wait.go
@@ -4,31 +4,12 @@
 
 package ldsync
 
-import (
-	"sync"
+type (
+	doneWaitWaitGroup = WaitGroup
+	doneWaitDone      = Done
 )
 
-type DoneWait interface {
-	Done
-
-	Add(n int)
-	Done()
-	Wait()
-}
-
-func NewDoneWait() DoneWait {
-	return newDoneWait()
-}
-
-func newDoneWait() *doneWait {
-	return &doneWait{
-		done: done{
-			c: make(chan struct{}),
-		},
-	}
-}
-
-type doneWait struct {
-	sync.WaitGroup
-	done
+type DoneWait struct {
+	doneWaitWaitGroup
+	doneWaitDone
 }
