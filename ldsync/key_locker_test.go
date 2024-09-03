e1ec52dbad035f575369266fc5cefb9e384941e9
distroy <distroy@163.com>
2024-09-03 14:02:06 +0800

    refactor(ldsync): refactor type `LockerPool`

--- ldsync/key_locker_test.go
+++ ldsync/key_locker_test.go
@@ -12,16 +12,23 @@ import (
 	"github.com/smartystreets/goconvey/convey"
 )
 
-func TestKeyLocker(t *testing.T) {
-	convey.Convey(t.Name(), t, func() {
-		kl := NewKeyLocker()
-		key1 := "123"
-		key2 := 123
+func TestLockerPool(t *testing.T) {
+	convey.Convey(t.Name(), t, func(c convey.C) {
+		kl := NewLockerPool()
+		key0 := "123"
+		key1 := 123
 
-		convey.So(kl.TryLock(key1), convey.ShouldBeTrue)
-		convey.So(kl.TryLock(key2), convey.ShouldBeTrue)
-		convey.So(kl.TryLock(key1), convey.ShouldBeFalse)
-		convey.So(kl.TryLock(key2), convey.ShouldBeFalse)
+		l00 := kl.Get(key0)
+		l10 := kl.Get(key1)
+		l01 := kl.Get(key0)
+		l11 := kl.Get(key1)
+
+		c.So(l00.TryLock(), convey.ShouldBeTrue)
+		c.So(l10.TryLock(), convey.ShouldBeTrue)
+		c.So(l00.TryLock(), convey.ShouldBeTrue)
+		c.So(l10.TryLock(), convey.ShouldBeTrue)
+		c.So(l01.TryLock(), convey.ShouldBeFalse)
+		c.So(l11.TryLock(), convey.ShouldBeFalse)
 
 		var key1UnlockTime, key1LockTime time.Time
 		var key2UnlockTime, key2LockTime time.Time
@@ -30,40 +37,40 @@ func TestKeyLocker(t *testing.T) {
 		wg.Add(4)
 		go func() {
 			defer wg.Done()
-			kl.Lock(key1)
+			l01.Lock()
 			key1LockTime = time.Now()
 		}()
 		go func() {
 			defer wg.Done()
 			time.Sleep(1 * time.Millisecond)
-			kl.Unlock(key1)
+			l00.Unlock()
 			key1UnlockTime = time.Now()
 		}()
 		go func() {
 			defer wg.Done()
-			kl.Lock(key2)
+			l11.Lock()
 			key2LockTime = time.Now()
 		}()
 		go func() {
 			defer wg.Done()
 			time.Sleep(100 * time.Millisecond)
-			kl.Unlock(key2)
+			l10.Unlock()
 			key2UnlockTime = time.Now()
 		}()
 		wg.Wait()
 
-		convey.So(key1UnlockTime, convey.ShouldHappenBefore, key1LockTime)
-		convey.So(key2UnlockTime, convey.ShouldHappenBefore, key2LockTime)
-		convey.So(key1LockTime, convey.ShouldHappenBefore, key2LockTime)
-		convey.So(key1UnlockTime, convey.ShouldHappenBefore, key2UnlockTime)
+		c.So(key1UnlockTime, convey.ShouldHappenBefore, key1LockTime)
+		c.So(key2UnlockTime, convey.ShouldHappenBefore, key2LockTime)
+		c.So(key1LockTime, convey.ShouldHappenBefore, key2LockTime)
+		c.So(key1UnlockTime, convey.ShouldHappenBefore, key2UnlockTime)
 
-		kl.Unlock(key1)
-		kl.Unlock(key2)
-		kl.Unlock(key2) // double unlock
+		l01.Unlock()
+		l11.Unlock()
+		l11.Unlock() // double unlock
 
-		convey.So(kl.TryLock(key1), convey.ShouldBeTrue)
-		convey.So(kl.TryLock(key2), convey.ShouldBeTrue)
-		convey.So(kl.TryLock(key1), convey.ShouldBeFalse)
-		convey.So(kl.TryLock(key2), convey.ShouldBeFalse)
+		c.So(l00.TryLock(), convey.ShouldBeTrue)
+		c.So(l10.TryLock(), convey.ShouldBeTrue)
+		c.So(l01.TryLock(), convey.ShouldBeFalse)
+		c.So(l11.TryLock(), convey.ShouldBeFalse)
 	})
 }
